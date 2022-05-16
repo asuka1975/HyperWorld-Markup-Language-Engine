71ba0ec4c33d2f894dd06eb51e49691c88b51b06
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-05-16 16:39:51 +0900

    feat: create empty window
    gui library: gtk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate gtk;
+
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("com.github.asuka1975.HyperWorld-Markup-Language-Engine")
+        .build();
+
+    app.connect_activate(|app| {
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(800)
+            .default_height(500)
+            .title("HyperWorld-Markup-Language-Engine")
+            .build();
+        win.show_all();
+    });
+
+    app.run();
 }
